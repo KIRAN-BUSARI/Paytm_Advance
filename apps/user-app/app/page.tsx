0e9c19e62d0042ec948835fe680c80b0a475769f
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-04 20:50:04 +0530

    Initialised the user-app and also the tailwind and also few api's

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="text-5xl items-center h-screen flex justify-center bg-clip-text from-neutral-100 to-neutral-500 text-transparent bg-gradient-to-b">Hello from User-app</div>
+  );
+}
