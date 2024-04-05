6f0f165a5cdd9ebc97fcf92633f2869b31841774
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-05 18:59:36 +0530

    Add the getserversession and authoptions

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,16 +1,37 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db/client";
+// import { NextResponse } from "next/server";
+// import { PrismaClient } from "@repo/db/client";
 
-const client = new PrismaClient();
+// const client = new PrismaClient();
+
+// export const GET = async () => {
+//   const user = await client.user.findMany({
+//     where: {
+//       email: "kirankishore@gmail.com",
+//     },
+//   });
+//   return NextResponse.json({
+//     message: "User Fetched",
+//     User: user,
+//   });
+// };
+
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-  await client.user.create({
-    data: {
-      email: "kiran1kish@gmail.com",
-      name: "adsads",
+  const session = await getServerSession(authOptions);
+  if (session.user) {
+    return NextResponse.json({
+      user: session.user,
+    });
+  }
+  return NextResponse.json(
+    {
+      message: "You are not logged in",
     },
-  });
-  return NextResponse.json({
-    message: "hi there",
-  });
+    {
+      status: 403,
+    }
+  );
 };
