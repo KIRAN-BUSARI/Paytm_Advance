665f29505debb3521107a5164b2acd3578946f29
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-05 19:12:41 +0530

    merchant config files

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(): JSX.Element {
+  return (
+    <div className="text-5xl items-center h-screen flex justify-center bg-clip-text from-neutral-100 to-neutral-500 text-transparent bg-gradient-to-b">Hello from Merchant-app</div>
+  );
+}
