8805fcf79ec468111e4093e4944eaa1bf8e78707
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-05 19:08:32 +0530

    Add Appbar and next-auth

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,15 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "../../../packages/ui/src/Appbar";
 
 export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-5xl items-center h-screen flex justify-center bg-clip-text from-neutral-100 to-neutral-500 text-transparent bg-gradient-to-b">Hello from User-app</div>
+    <>
+      <div className="">
+        <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+      </div>
+      <div className="text-5xl items-center h-screen flex justify-center bg-clip-text from-neutral-100 to-neutral-500 text-transparent bg-gradient-to-b">Hello from User-app</div>
+    </>
   );
 }
