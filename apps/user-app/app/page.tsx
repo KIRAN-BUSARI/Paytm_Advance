28593afe3ed857194fcafd01ae8d5cf0c41bc996
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-06 23:54:59 +0530

    remove extra

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,15 +1,12 @@
-"use client"
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "../../../packages/ui/src/Appbar";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-export default function Page(): JSX.Element {
-  const session = useSession();
-  return (
-    <>
-      <div className="">
-        <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-      </div>
-      <div className="text-5xl items-center h-screen flex justify-center bg-clip-text from-neutral-100 to-neutral-500 text-transparent bg-gradient-to-b">Hello from User-app</div>
-    </>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
 }
